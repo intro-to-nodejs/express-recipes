f9447c3eeba3967dd89f567f8c21d4471f5aed48
kchia <kchia87@gmail.com>
2020-06-28 11:51:29 -0400

    add GET / route handler

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('Welcome to Express Recipes!');
+});
+
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server is up on port ${port}.`);
