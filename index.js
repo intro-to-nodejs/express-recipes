e5717b589dd1d19f8f8d0fa5fef5cda4130d1792
kchia <kchia87@gmail.com>
2020-06-28 13:06:40 -0400

    serve static files to the client

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Welcome to Express Recipes!');
-});
+/**
+  MIDDLEWARE
+*/
+const publicDirectoryPath = path.join(__dirname, './public');
+app.use(express.static(publicDirectoryPath));
 
+/*
+  ROUTE HANDLERS
+*/
 app.get('/:name', (req, res) => {
   res.send(`Welcome to Express Recipes, ${req.params.name}!`);
 });
