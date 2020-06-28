2f4a9c87b8ef928bd713dd0ed97fae1737abe427
kchia <kchia87@gmail.com>
2020-06-28 12:02:05 -0400

    add GET /:name handler

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Welcome to Express Recipes!');
 });
 
+app.get('/:name', (req, res) => {
+  res.send(`Welcome to Express Recipes, ${req.params.name}!`);
+});
+
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server is up on port ${port}.`);
