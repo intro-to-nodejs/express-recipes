a69c22d63db5981ddd31d27ed4e7c222e637f702
kchia <kchia87@gmail.com>
2020-06-28 21:24:16 -0400

    implement GET / to retrieve all recipes

--- src/routers/recipe.js
+++ src/routers/recipe.js
@@ -1,4 +1,18 @@
 const express = require('express');
+const fs = require('fs').promises;
+const path = require('path');
+
 const router = express.Router();
+
+const recipesFilePath = path.join(__dirname, '../recipes.json');
+
+router.get('/', async (req, res, next) => {
+  try {
+    const data = await fs.readFile(recipesFilePath);
+    res.json(JSON.parse(data));
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
